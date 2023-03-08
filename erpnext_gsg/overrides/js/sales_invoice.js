5b578ef13fd3f19f23ed42dd5f594b9246680181
e.mabunada <e.mabunada@gmail.com>
2023-03-08 15:01:19 +0200

    4. Create new Stock Entry

--- erpnext_gsg/overrides/js/sales_invoice.js
+++ erpnext_gsg/overrides/js/sales_invoice.js
@@ -2,7 +2,7 @@ frappe.ui.form.on('Sales Invoice',  {
     refresh: function(frm) {
     alert("asdsads")
       frappe.call({
-      method: "erpnext_gsg.events.vat_tax_event.create_sales_vat_template",
+      method: "erpnext_gsg.events.vat_tax_events.create_sales_vat_template",
       args: {},
       success: function (r) {},
     });
